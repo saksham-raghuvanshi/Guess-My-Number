4afb82ff30490114435b31ae1f5d33b040b7a2c3
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-30 14:22:15 +0530

    Guess fix

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const seceretScore = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = seceretScore;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === seceretScore) {
+    document.querySelector('.message').textContent = '💐 Correct Number';
+  } else if (guess > seceretScore) {
+    document.querySelector('.message').textContent = '📈 Too High';
+  } else if (guess < seceretScore) {
+    document.querySelector('.message').textContent = '📉 Too Low';
   }
 });
